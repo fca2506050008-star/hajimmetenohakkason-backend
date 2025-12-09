11a3568ea82c2bf2685059a643f88c4167ec4c61
fca2506050008-star <fca2506050008@edu.fca.ac.jp>
2025-12-09 11:01:50 +0900

    login_jwt

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import Session
 from .db import get_db
 from .models import User
 from .schemas import LoginRequest, TodoUpdate
+from datetime import datetime, timedelta
+from jose import jwt
 
 app = FastAPI()
 
@@ -12,9 +14,31 @@ def hello():
 	
 @app.post("/login")
 def login(body: LoginRequest, db: Session = Depends(get_db)):
+
+	# ユーザーがDBにいるかを調査
 	user = db.query(User).filter(User.email == body.email).first()
+
+	# ユーザーがいなかった場合新規作成
 	if user == None:
 		new_user = User(email=body.email, password=body.password)
 		db.add(new_user)
-		db.commit()
-		return db.query(User).all()
+		db.commit()	
+
+	access_token = create_access_token(
+        data={"sub": str(user.id)}
+	)
+
+	return {"access_token": access_token, "token_type": "bearer"}
+
+
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+	
